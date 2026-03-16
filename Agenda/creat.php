0c7637d4d05654e9d635237b4a451e6d7726c4c1
Kerlley Pacheco <kerlleypachecol@gmail.com>
2026-03-16 20:34:32 -0300

    Upload project files
    Added initial project structure and files for the contact manager application.

---
+++ Agenda/creat.php
@@ -0,0 +1,29 @@
+<?php
+
+    include_once("templates/header.php");
+?>
+
+    <div class="container">
+        <?php include_once("templates/backbtn.html"); ?>
+        <h1 id="main-title">Criando contatos</h1>
+        <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="post">
+            <input type="hidden" name="type" value="create">
+            <div class="form-group">
+                <label for="name">Nome do contato</label>
+                <input type="text" class="form-control" name="name" id="name" placeholder="Digite seu nome" required>
+            </div>
+            <div class="form-group">
+                <label for="phone">Telefone do contato</label>
+                <input type="text" class="form-control" name="phone" id="phone" placeholder="Digite o telefone" required>
+            </div>
+            <div class="form-group">
+                <label for="observations">Observações</label>
+                <textarea type="text" class="form-control" name="observations" id="observations"  placeholder="Observações" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </form>
+    </div>
+
+<?php 
+    include_once("templates/footer.php");
+?>
---
+++ Agenda/templates/header.php
@@ -0,0 +1,38 @@
+<?php
+    include_once('config/url.php');
+    include_once('config/process.php');
+
+    //limpa a mensagem quando recarregar a pagina
+    if(isset($_SESSION['msg'])) {
+        $printMsg = $_SESSION['msg'];
+        $_SESSION['msg'] = '';
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda de contatos</title>
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.8/css/bootstrap.min.css" integrity="sha512-2bBQCjcnw658Lho4nlXJcc6WkV/UxpE/sAokbXPxQNGqmNdQrWqtw26Ns9kFF/yG792pKR1Sx8/Y1Lf1XN4GKA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="<?= $BASE_URL ?>index.php">
+                <img src="<?= $BASE_URL ?>img/logo.svg" alt="Agenda">
+            </a>
+            <div>
+                <div class="navbar-nav">
+                    <a class="nav-link active" href="<?= $BASE_URL ?>index.php">Agenda</a>
+                    <a class="nav-link active" id="home-link" href="<?= $BASE_URL ?>creat.php">Adicionar</a>
+                </div>
+            </div>
+        </nav>
+    </header>
